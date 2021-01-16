9bcd5118127f02dde18ff73b2f1aa54e91e2fd54
Aswin A V <aswinavofficial@gmail.com>
2021-01-16 11:51:06 +0530

    Customizable timings & layout changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 export default function App() {
 
-  const [timeLeft,setTimeLeft] = useState(25 *60)
+  const[totalMinutes,setTotalMinutes] = useState(25)
+  const[totalSeconds,setTotalSeconds] = useState(0)
+  const[timeLeft,setTimeLeft] = useState(totalMinutes * 60 + totalSeconds)
+  const[timerRunning,setTimerRunning] = useState(false)
   let interval = useRef(null)
 
   function padZero(time) {
@@ -13,12 +16,18 @@ export default function App() {
   function startTimer() {
 
     if(interval.current ==null) {
-    interval.current = setInterval(()=> {
+
+      setTimerRunning(true)
+      setTimeLeft(totalMinutes * 60 + totalSeconds)
+      interval.current = setInterval(()=> {
       setTimeLeft(timeLeft => {
 
         if(timeLeft>= 1)
         return timeLeft - 1;
 
+        setTimerRunning(false)
+        clearInterval(interval.current);
+        setTimeLeft(totalMinutes * 60 + totalSeconds);
         return 0;
       })
     },1000);
@@ -28,14 +37,16 @@ export default function App() {
 
   function stopTimer() {
 
+    setTimerRunning(false)
     clearInterval(interval.current);
     interval.current = null;
   }
 
   function resetTimer() {
 
+    setTimerRunning(false)
     clearInterval(interval.current);
-    setTimeLeft(25*60);
+    setTimeLeft(totalMinutes * 60 + totalSeconds);
     interval.current = null;
 
   }
@@ -46,14 +57,36 @@ export default function App() {
       <h2>Pomodoro!</h2>
 
       <div className="timer">
-      <span>{padZero(minutes)}</span>
-        <span>:</span>
-        <span>{padZero(seconds)}</span>
+      <span contentEditable={!timerRunning} 
+         suppressContentEditableWarning={true}
+          onInput={(e)=> {
+            setTotalMinutes(e.currentTarget.textContent)
+          }}>
+        {padZero(minutes)}
+      </span>
+
+      <span>:</span>
+      
+        <span contentEditable={!timerRunning} 
+         suppressContentEditableWarning={true}
+          onInput={(e)=> {
+            setTotalSeconds(e.currentTarget.textContent)
+          }}>
+        {padZero(seconds)}
+      </span>
       </div>
 
       <div className="buttons">
-        <button onClick = {startTimer}>Start</button>
-        <button onClick ={stopTimer}>Stop</button>
+        <button 
+          onClick = {startTimer}
+          disabled={timerRunning}>
+          Start
+        </button>
+        <button 
+          onClick ={stopTimer}
+          disabled={!timerRunning}>
+          Stop
+          </button>
         <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
